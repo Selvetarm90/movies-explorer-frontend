d202376311bf02433c94811bfb2503f9d8ada31f
Aleksey Paramonov <paramonovav90@gmail.com>
2022-09-27 13:00:32 +0400

    popup done

--- src/components/NavigatePopup/NavigatePopup.js
+++ src/components/NavigatePopup/NavigatePopup.js
@@ -6,7 +6,7 @@ export default function NavigatePopup(props) {
     <div className={`popup ${props.isOpen ? 'popup_opened' : ''}`}>
       <button type='button' className='popup__button-close' onClick={props.onClose}/>
       <nav className='popup__navigate'>
-        <NavLink className='popup__link' activeClassName='popup__link_active' to='/'>Главная</NavLink>
+        <NavLink className='popup__link' activeClassName='popup__link_active' exact to='/'>Главная</NavLink>
         <NavLink className='popup__link' activeClassName='popup__link_active' to='movies'>Фильмы</NavLink>
         <NavLink className='popup__link' activeClassName='popup__link_active' to='saved-movies'>Сохраненные фильмы</NavLink>
         <Link className='popup__link popup__link_el_account' to='profile'>Аккаунт</Link>
