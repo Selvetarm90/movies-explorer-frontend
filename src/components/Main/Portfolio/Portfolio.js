88f89cd24bc9dda95033e616729db25ebe701f6c
Aleksey Paramonov <paramonovav90@gmail.com>
2022-10-05 13:14:33 +0400

    review-1

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -6,31 +6,34 @@ export default function Portfolio() {
       <h2 className='portfolio__heading'>Портфолио</h2>
       <ul className='portfolio__links'>
         <li className='portfolio__link'>
-          <p className='portfolio__link-name'>Статичный сайт</p>
           <a
             href='https://github.com/Selvetarm90/how-to-learn'
             target='_blank'
             rel='noreferrer'
             className='portfolio__link-arrow'
-          />
+          >
+            <p className='portfolio__link-name'>Статичный сайт</p>
+          </a>
         </li>
         <li className='portfolio__link'>
-          <p className='portfolio__link-name'>Адаптивный сайт</p>
           <a
             href='https://github.com/Selvetarm90/russian-travel'
             target='_blank'
             rel='noreferrer'
             className='portfolio__link-arrow'
-          />
+          >
+            <p className='portfolio__link-name'>Адаптивный сайт</p>
+          </a>
         </li>
         <li className='portfolio__link'>
-          <p className='portfolio__link-name'>Одностраничное приложение</p>
           <a
             href='https://github.com/Selvetarm90/react-mesto-api-full'
             target='_blank'
             rel='noreferrer'
             className='portfolio__link-arrow'
-          />
+          >
+            <p className='portfolio__link-name'>Одностраничное приложение</p>
+          </a>
         </li>
       </ul>
     </section>
--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -4,7 +4,7 @@ import './SearchForm.css';
 export default function SearchForm() {
   return (
     <form className='search-form'>
-      <input type='text' className='search-form__input' placeholder='Фильм' />
+      <input type='text' className='search-form__input' required placeholder='Фильм' />
       <button className='search-form__button' type='submit'>
         Найти
       </button>
